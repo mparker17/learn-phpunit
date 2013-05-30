e64d71b3942de8281d1d67838db2e07c8a21598b
M Parker <mparker17@536298.no-reply.drupal.org>
2013-05-30 16:55:40 -0400

    Keep track of score

--- src/Mpd/BowlingKata/BowlingGame.php
+++ src/Mpd/BowlingKata/BowlingGame.php
@@ -4,7 +4,13 @@ namespace Mpd\BowlingKata;
 
 class BowlingGame {
 
+    private $score;
+
     public function score() {
-        return 0;
+        return $this->score;
+    }
+
+    public function roll($pins) {
+        $this->score += $pins;
     }
 }
--- tests/BowlingGameTest.php
+++ tests/BowlingGameTest.php
@@ -3,9 +3,21 @@
 use \Mpd\BowlingKata\BowlingGame;
 
 class BowlingGameTest extends PHPUnit_Framework_TestCase {
-
     function testBowlAllZeroes() {
         $game = new BowlingGame();
+
+        // Roll a bunch of zeroes.
+        $game->roll(0);
+        $game->roll(0);
+        $game->roll(0);
+        $game->roll(0);
+        $game->roll(0);
+        $game->roll(0);
+        $game->roll(0);
+        $game->roll(0);
+        $game->roll(0);
+        $game->roll(0);
+
         $this->assertSame(0, $game->score());
     }
 }
